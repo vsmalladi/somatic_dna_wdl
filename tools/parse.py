2545aab1fca59f334587e4629b77e44cf1718ad9
Jennifer Shelton <jshelton@nygenome.org>
2021-02-21 15:16:32 -0500

    feat: get inputs from wom object

--- tools/parse.py
+++ tools/parse.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pprint
 import json
+import subprocess
 
 class Wdl():
     '''draft input JSON from various sources
@@ -15,16 +16,18 @@ class Wdl():
         self.genome = genome
         self.inputs = {}
         self.input_objects = {}
-        self.wf_name = self.load_wf_name(wdl_file)
         if project_info_file:
             self.project_info = self.load_json(project_info_file)
         else:
             self.project_info = {}
         # find required input
-        for type, variable in self.load(wdl_file):
+        for type, variable, full_variable, default in self.load(wdl_file):
             self.inputs[variable] = {'variable' : variable,
+                                     'default' : default,
+                                     'full_variable' : full_variable,
                                      'object' : None,
                                      'type' : type}
+        self.wf_name = self.load_wf_name()
         # load preexisting reference variables
         self.load_genome_input(genome_input)
         # populate
@@ -34,10 +37,12 @@ class Wdl():
     def finish_inputs(self):
         final_inputs = {}
         for variable in self.inputs:
-            try:
-                final_inputs[self.wf_name + '.' + variable] = self.inputs[variable]['object']
-            except TypeError:
-                final_inputs[self.wf_name + '.' + variable] = self.inputs[variable]
+            if not self.inputs[variable]['default']:
+                full_variable = self.inputs[variable]['full_variable']
+                try:
+                    final_inputs[full_variable] = self.inputs[variable]['object']
+                except TypeError:
+                    final_inputs[full_variable] = self.inputs[variable]
         self.inputs = final_inputs
         
     def add_from_ref(self):
@@ -46,7 +51,8 @@ class Wdl():
             # skip resource vars because these will be hardcoded
             if not variable.endswith('DockerImage') and not variable.endswith('threads') and not variable.endswith('Mem') and not variable.endswith('mem'):
                 if variable in self.input_objects:
-                    self.inputs[variable] = self.input_objects[variable]
+                    self.inputs[variable]['object'] = self.input_objects[variable]['object']
+                    self.inputs[variable]['default'] = False
                     
     def add_from_project(self):
         '''Load from user define project-specific information 
@@ -57,7 +63,8 @@ class Wdl():
             if not variable.endswith('DockerImage') and not variable.endswith('threads') and not variable.endswith('Mem') and not variable.endswith('mem'):
                 if not self.inputs[variable]['object']:
                     if variable in self.project_info:
-                        self.inputs[variable] = self.project_info[variable]
+                        self.inputs[variable]['object'] = self.project_info[variable]
+                        self.inputs[variable]['default'] = False
                     
     def populate_inputs(self):
         self.add_from_ref()
@@ -71,13 +78,8 @@ class Wdl():
             return True, False, False
         if components[0] == '}':
             return False, False, False
-        if len(components) == 2:
-            return [True] + components
-        if len(components) > 2:
-            # skip lines where input variables are 
-            # composed from other input variables with an 
-            # equal sign
-            return True, False, False
+        if len(components) >= 2:
+            return [True] + components[0:2]
         
     def load_json(self, file):
         with open(file) as input:
@@ -89,18 +91,35 @@ class Wdl():
         data = self.load_json(file)[self.genome]
         assert len(set(data.keys()).intersection(self.input_objects.keys())) == 0, 'inputs must be unique'
         for variable in data:
-            self.input_objects[variable] = data[variable]
-            
+            if variable in self.inputs:
+                self.input_objects[variable] = data[variable]
     
-    def load_wf_name(self, file):
-        with open(file) as input:
-            for line in input:
-                if line.startswith('workflow '):
-                    components = line.split()
-                    name = components[1]
-                    return name
-                      
+    def load_wf_name(self):
+        return [self.inputs[variable]['full_variable'].split('.')[0] for variable in self.inputs][0]
+                
     def load(self, file):
+        '''Use womtool to list input files
+        Replace with python parser later'''
+        custom_types = {variable: type for type, variable in self.load_custom_type(file)}
+        try:
+            result = subprocess.run(['womtool', 'inputs', file],
+                                    check=True,
+                                    stdout=subprocess.PIPE).stdout.decode('utf-8')
+        except CalledProcessError:
+            log.error('Failed to read file: ' + file)
+        inputs = json.loads(result)
+        for input in inputs:
+            variable = input.split('.')[-1]
+            # skip variables only defined with default
+            if variable in custom_types:
+                type = custom_types[variable]
+                full_variable = input
+                default = 'default =' in inputs[input]
+                yield type, variable, full_variable, default
+        
+                      
+    def load_custom_type(self, file):
+        ''' get name of custom struct from WDL file'''
         read = False
         with open(file) as input:
             for line in input:
