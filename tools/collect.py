7ecc2ee727008800e3fec4521495ce529289f143
Jennifer Shelton <jshelton@nygenome.org>
2021-03-04 15:13:16 -0500

    feat: output pair_association and sample_association maps

--- tools/collect.py
+++ tools/collect.py
@@ -13,25 +13,104 @@ import logging as log
 import Colorer
 from collections.abc import Iterable
 import copy
+from _ast import Or
+import meta
 
 log.basicConfig(format='%(levelname)s:  %(message)s', level=log.INFO)
 
 
-import meta
 
 
-class JsonUpdate():
+class JsonModify():
     
-    def __init__(self, inputs, dir):
-        '''deep copy required to modify complex outputs structure while
-        keeping an original'''
+    def __init__(self, inputs, 
+                 sample_id=False, 
+                 pair_id=False,
+                 dir=False,
+                 pair_ids=False,
+                 all_pair_ids=False,
+                 all_sample_ids=False,
+                 tool='match'):
+        '''
+        deep copy required to modify complex outputs structure while
+        keeping an original
+        tool options:
         
-        self.dir = dir
+        skip: skip objects that have no relevant files
+        convert_path: swaps path betore the task call- directory out with
+        the final output directory
+        match: Assumes that the sample name followed by "." or "_" are in the basename
+            Sorts out which objects have names that refer to a pair or sample
+        '''
+        self.tool = tool
+        self.sample_id = sample_id
+        self.pair_id = pair_id
         self.json_obj = copy.deepcopy(inputs)
         self.final_json_obj = copy.deepcopy(inputs)
+        self.dir = dir
         self.files = []
+        self.other_file_pair_ids = []
+        if all_pair_ids:
+            self.other_file_pair_ids = [pair_id + '.' for pair_id in all_pair_ids if 
+                                        (not pair_id == self.pair_id)
+                                        and (len(pair_id) > len(self.pair_id))]
+            self.other_file_pair_ids += [pair_id + '_' for pair_id in all_pair_ids if 
+                                        (not pair_id == self.pair_id)
+                                        and (len(pair_id) > len(self.pair_id))]
+        self.other_file_sample_ids = []
+        if all_sample_ids:
+            self.other_file_sample_ids = [sample_id + '.' for sample_id in all_sample_ids if 
+                                          (not sample_id == self.sample_id) 
+                                          and (len(sample_id) > len(self.sample_id))]
+            self.other_file_sample_ids += [sample_id + '_' for sample_id in all_sample_ids if 
+                                          (not sample_id == self.sample_id) 
+                                          and (len(sample_id) > len(self.sample_id))]
+        if self.tool == 'convert_path':
+            assert self.dir, 'dir is required when tool is convert_path'
+        elif self.tool in ['match']:
+            if pair_ids:
+                self.file_pair_ids = [pair_id + '.' for pair_id in pair_ids]
+                self.file_pair_ids += [pair_id + '_' for pair_id in pair_ids]
+            else:
+                self.file_pair_ids = []
+            assert self.sample_id or self.pair_id, 'pair_id or sample_id is required when tool is match'
+        elif self.tool == 'skip':
+            pass
+        else:
+            log.error('tool options are match, skip, and convert_path: ' + tool)
         self.hlpr_fnc(self.json_obj, self.final_json_obj)
-        print(self.final_json_obj)
+        if self.tool == 'skip':
+            self.skip = len(self.files) == 0
+    
+    def skip(self, string):
+        '''skip objects that have no relevant files'''
+        if string.endswith('...'):
+            # LongObject'
+            return False
+        elif not string.startswith('gs://'):
+            return False
+        else:
+            uri = string
+            basename = uri.split('/')[-1]
+            self.files.append(uri)
+            return True
+        
+    def match(self, string):
+        if not string.startswith('gs://'):
+            return string
+        else:
+            uri = string
+            basename = uri.split('/')[-1]
+            if not self.sample_id:
+                if self.pair_id + '.' in basename or self.pair_id + '_' in basename:
+                    if not any([file_sample_id in basename for file_sample_id in self.other_file_sample_ids]):
+                        return uri
+            else:
+                if not any([file_pair_id in basename for file_pair_id in self.file_pair_ids]):
+                    if self.sample_id + '.' in basename or self.sample_id + '_' in basename:
+                        if not any([file_pair_id in basename for file_pair_id in self.other_file_pair_ids]):
+                            return uri
+            return False
         
     def convert_path(self, string):
         if string.endswith('...'):
@@ -42,8 +121,11 @@ class JsonUpdate():
         else:
             uri = string
             basename = '/'.join(uri.split('/call-')[-1].split('/')[1:])
-            self.files.append(self.dir + '/' + basename)
-            return self.dir + '/' + basename
+            new_uri = self.dir + '/' + basename
+            new_uri = '/'.join([section for section in new_uri.split('/') 
+                               if not section.startswith('attempt-') ])
+            self.files.append(new_uri)
+            return new_uri
         
     def hlpr_fnc_list(self, list1, description):
         for i, subnode in enumerate(list1):
@@ -53,7 +135,12 @@ class JsonUpdate():
                 self.hlpr_fnc_list(subnode, description[i])
             else:
                 if isinstance(subnode, str):
-                    description[i] = self.convert_path(subnode)
+                    if self.tool == 'convert_path':
+                        description[i] = self.convert_path(subnode)
+                    elif self.tool == 'match':
+                        description[i] = self.match(subnode)
+                    elif self.tool == 'skip':
+                        self.skip(subnode)
                 
     def hlpr_fnc(self, dict1, description):
         nodes = dict1.keys()
@@ -65,55 +152,14 @@ class JsonUpdate():
                 self.hlpr_fnc_list(subnode, description[node])
             else:
                 if isinstance(subnode, str):
-                    description[node] = self.convert_path(subnode)
-
-class JsonMatch():
-    
-    def __init__(self, inputs, sample_id, pair_id):
-        '''Assumes that the sample name followed by "." or "_" are in the basename'''
-        self.sample_id = sample_id
-        self.pair_id = pair_id
-        self.json_obj = copy.deepcopy(inputs)
-        self.final_json_obj = copy.deepcopy(inputs)
-        self.files = []
-        self.hlpr_fnc(self.json_obj, self.final_json_obj)
-        
-    def match(self, string):
-        if not string.startswith('gs://'):
-            return string
-        else:
-            uri = string
-            basename = uri.split('/')[-1]
-            if self.pair_id:
-                if self.pair_id + '.' in basename or self.pair_id + '_' in basename:
-                    return uri
-            else:
-                if self.sample_id+ '.' in basename or self.sample_id + '_' in basename:
-                    return uri
-            return False
-        
-    def hlpr_fnc_list(self, list1, description):
-        for i, subnode in enumerate(list1):
-            if isinstance(subnode, dict):
-                self.hlpr_fnc(subnode, description[i])
-            elif not isinstance(subnode, str) and isinstance(subnode, Iterable):
-                self.hlpr_fnc_list(subnode, description[i])
-            else:
-                if isinstance(subnode, str):
-                    description[i] = self.match(subnode)
-                
-    def hlpr_fnc(self, dict1, description):
-        nodes = dict1.keys()
-        for node in nodes:
-            subnode = dict1[node]
-            if isinstance(subnode, dict):
-                self.hlpr_fnc(subnode, description[node])
-            elif not isinstance(subnode, str) and isinstance(subnode, Iterable):
-                self.hlpr_fnc_list(subnode, description[node])
-            else:
-                if isinstance(subnode, str):
-                    description[node] = self.match(subnode)
-
+                    if self.tool == 'convert_path':
+                        description[node] = self.convert_path(subnode)
+                    elif self.tool == 'match':
+                        description[node] = self.match(subnode)
+                    elif self.tool == 'skip':
+                        self.skip(subnode)
+                    
+                    
 class CloudOutput():
     def __init__(self, run_data):
         self.run_data = run_data
@@ -123,24 +169,57 @@ class CloudOutput():
         self.gather_outputs()
         self.unnamed_files = self.gather_unnamed()
         self.divide_by_id()
-#         self.run_data.run_info['pair_association'] = self.pair_association
-#         self.run_data.run_info['sample_association'] = self.sample_association
+        self.run_data.run_info['pair_association'] = self.pair_association
+        self.run_data.run_info['sample_association'] = self.sample_association
         self.run_data.run_info['outputs'] = self.named_outputs
         self.run_data.run_info['named_files'] = self.named_files
         self.run_data.run_info['unnamed_files'] = self.unnamed_files
         
+    def filter_by_id(self, association, pair=True, sample=False):
+        '''skip object that have no files after filtering by id'''
+        association = copy.deepcopy(association)
+        new_association = {}
+        for id in association:
+            for object in association[id]:
+                association_object = copy.deepcopy(association[id][object])
+                if pair:
+                    skip = JsonModify(inputs={object: association_object},
+                                      pair_id=id,
+                                      tool='skip')
+                if sample:
+                    skip = JsonModify(inputs={object: association_object},
+                                      sample_id=id,
+                                      tool='skip')
+                if not skip.skip:
+                    if not id in new_association:
+                        new_association[id] = {}
+                    new_association[id][object] = association_object
+        return new_association
+        
     def divide_by_id(self):
-        self.pair_association = {}
-        self.sample_association = {}
+        pair_association = {}
+        sample_association = {}
         pair_ids = list(set([pair_info["pairId"] for pair_info in self.run_data.project_info["pairInfos"]]))
         for pair_id in pair_ids:
-            self.pair_association[pair_id] = JsonMatch(inputs=self.named_outputs, 
-                                                       sample_id=False, 
-                                                       pair_id=pair_id).final_json_obj
+            pair_association[pair_id] = JsonModify(inputs=self.named_outputs, 
+                                                    all_pair_ids=pair_ids,
+                                                    pair_id=pair_id,
+                                                    tool='match').final_json_obj
         for sample_id in self.run_data.project_info["sampleIds"]:
-            self.sample_association[sample_id] = JsonMatch(inputs=self.named_outputs, 
-                                                           sample_id=sample_id, 
-                                                           pair_id=False).final_json_obj
+            pair_ids = [pair_info["pairId"] for pair_info in self.run_data.project_info["pairInfos"] 
+                        if pair_info['tumor'] ==  sample_id or 
+                        pair_info['normal'] ==  sample_id]
+            sample_association[sample_id] = JsonModify(inputs=self.named_outputs,
+                                                       all_sample_ids=self.run_data.project_info["sampleIds"],
+                                                       sample_id=sample_id,
+                                                       pair_ids=pair_ids,
+                                                       tool='match').final_json_obj
+        self.pair_association = self.filter_by_id(pair_association, 
+                                                  pair=True, 
+                                                  sample=False)
+        self.sample_association = self.filter_by_id(sample_association, 
+                                                    pair=False, 
+                                                    sample=True)
 
     def out_path(self):
         if self.options["use_relative_output_paths"]:
@@ -152,8 +231,9 @@ class CloudOutput():
         
     def parse_block(self):
         self.out_dir =  self.out_path()
-        outputs = JsonUpdate(inputs=self.raw_outputs, 
-                              dir=self.out_dir)
+        outputs = JsonModify(inputs=self.raw_outputs, 
+                              dir=self.out_dir,
+                              tool='convert_path')
         self.named_outputs = outputs.final_json_obj
         self.named_files = outputs.files
         
@@ -212,9 +292,10 @@ class CloudOutput():
         
         
 class RunData():
-    def __init__(self, run_info_file, options_file):
+    '''Load rundata from file and confirm project data schema '''
+    def __init__(self, run_info_file):
         self.run_info = self.read(run_info_file)
-        self.options = self.run_info['options']
+        self.options = self.run_info['project_data']['options']
         self.project_info = self.run_info['project_data']
         self.passed = meta.test_schema(self.project_info)
         self.workflow_uuid = self.run_info['workflow_uuid']
@@ -238,11 +319,11 @@ def main():
             
 def get_args():
     '''Parse input flags
-        Need to add optional task-specific input json
     '''
     parser = argparse.ArgumentParser()
     parser.add_argument('--run-data',
                         help='JSON file with RunIfon. Includes workflow uuid, '
+                        'options JSON dictionary submitted to cromwell, '
                         'project pairing, sample, '
                         'genome build, library and interval list information',
                         required=True
