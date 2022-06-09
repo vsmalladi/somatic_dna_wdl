8cf37c3b6a741cbf0c14656577647f55812cf30e
Jennifer Shelton <jshelton@nygenome.org>
2022-06-09 13:10:06 -0400

    style: add avg prefix to averaged cost columns

--- tools/join.py
+++ tools/join.py
@@ -18,11 +18,10 @@ def is_preemptible(service):
 
 
 def formatter(service, drop=[], truncate=False, suffix=False):
-    # takes a string service type, split to list of words, drop some useless words,
-    # drop up to truncate word and suffix. can't just keep it do to 'storage'-> 'capacity'
+    '''takes a string service type, split to list of words, drop some useless words,
+    drop up to truncate word and suffix. can't just keep it do to 'storage'-> 'capacity' '''
     word_list = service.split()
     clean_words = [a for a in word_list if a not in drop]
-
     if truncate:
         try:
             truncate_index = clean_words.index(truncate)
@@ -31,14 +30,12 @@ def formatter(service, drop=[], truncate=False, suffix=False):
         words = clean_words[:truncate_index]
     if suffix:
         words.append(suffix)
-
     return '_'.join(words)
 
 
 def mapper(row):
-    # cost_type & preemptible are pretty clearly defined. cost_description not so much
+    '''cost_type & preemptible are pretty clearly defined. cost_description not so much'''
     service_type = row['service_type'].lower()
-
     if 'storage' in service_type or 'capacity' in service_type:
         cost_type = 'capacity_cost'
         preemptible = np.nan
@@ -72,23 +69,19 @@ def mapper(row):
         cost_type = 'unknown_cost'
         cost_description = 'unknown'
         preemptible = np.nan
-
     return cost_type, cost_description, preemptible
 
 
 def load_runtime(file, costs, uuid):
     runtime = pd.read_csv(file)
-
     runtime['main_workflow_id'] = uuid
     runtime['cromwell_workflow_id'] = 'cromwell-' + runtime['main_workflow_id']
-
     unique_roots = runtime.drop_duplicates(['main_workflow_name', 'cromwell_workflow_id']).set_index(
         'cromwell_workflow_id')
     costs['sub_workflow_name'] = np.where(costs['sub_workflow_name'].isna(),
                                           costs['cromwell_workflow_id'].map(unique_roots['main_workflow_name']),
                                           costs['sub_workflow_name']
                                           )
-
     costs[['cost_type', 'cost_desc', 'preemptible']] = costs.apply(mapper, axis=1, result_type="expand")
     grouper = ['cromwell_workflow_id', 'sub_workflow_name', 'wdl_task_name']
     wide_costs = pd.pivot_table(costs,
@@ -98,33 +91,28 @@ def load_runtime(file, costs, uuid):
                                 aggfunc='sum',
                                 fill_value=0
                                 ).reset_index()
-
     runtime = pd.merge(runtime, wide_costs, on=grouper, how='outer')
-
     cost_opts = ['capacity_cost', 'core_cost', 'ram_cost', 'egress_cost', 'unknown_cost']
     cost_cols = [c for c in cost_opts if c in runtime.columns]
-
     # the costs are still for the full group, divide them evenly
     runtime['group_count'] = runtime.groupby(grouper)['task_call_name'].transform('count')
     for c in cost_cols:
         runtime[c] = runtime[c] / runtime['group_count']
     runtime['total_cost'] = runtime[cost_cols].sum(axis=1)
-
     runtime['group_cost'] = runtime.groupby(grouper)['total_cost'].transform('sum')
     runtime['group_runtime'] = runtime.groupby(grouper)['actual_runtime_m'].transform('sum')
     runtime['runtime_scaled_total_cost'] = runtime['group_cost'] * (runtime['actual_runtime_m'] / runtime['group_runtime'])
-
     # add the service type, treat as set. Almost always going to be a single type
     core_costs = costs[costs['cost_type'] == 'core_cost'].copy()
     service_types = core_costs.groupby(grouper).agg({'cost_desc': set}).reset_index()
     runtime = pd.merge(runtime, service_types, on=grouper, how='outer')
-
     missing = costs[costs['cost_type'] == 'unknown_cost']['service_type'].unique().tolist()
     if len(missing) > 0:
-        log.warning('Following service_types are not yet described in map. Add them to join.py: ')
+        log.warning('Following service_types are not yet described in mapper(). Add them to join.py: ')
         log.warning(', '.join(missing))
-
     runtime.drop(columns=['group_count', 'group_cost', 'group_runtime'], inplace=True)
+    cost_cols_map = {col : 'avg_' + col for col + ['total_cost'] in cost_cols}
+    runtime = runtime.rename(columns=cost_cols_map)
     return runtime
 
 
