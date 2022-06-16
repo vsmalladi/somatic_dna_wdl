85e368850a47bebd5a049c94337f205fd102100b
Jennifer Shelton <jshelton@nygenome.org>
2022-06-16 09:45:14 -0400

    feat: cost and vm features  - add details about preemptions and cost to reports  - add plots for cost and make vm description more detailed  - skip regenerating runtime metrics and start from billing     when using the --from-billing flag  - Add help menu for run_summary.sh

--- run_summary.sh
+++ run_summary.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# USAGE: run_summary.sh -u URL -n PROJECT_NAME -g GCP_PROJECT -d log_dir [-r RUNINFO_JSON] [-i UUID] [-b BILLING EXPORT] [-p PAIRS_FILE] [-s SAMPLES_FILE]
-# DESCRIPTION: submit workflow to cromwell.
-# Use -l for large jobs that may timeout.
-# Script requires jq, cromwell-tools, gcloud to be in the path
+# USAGE: run_summary.sh -u URL -n PROJECT_NAME -g GCP_PROJECT -d log_dir [-r RUNINFO_JSON] [-i UUID] [-b BILLING EXPORT] [--from-billing] [-p PAIRS_FILE] [-s SAMPLES_FILE]
+# DESCRIPTION: monitor or summarize cromwell workflow.
+# Script requires jq to be in the path
 # Script generates summary of any workflow.
-# If no *.RunInfo.json file is provided then
+# If no *.RunInfo.json OR uuid file is provided then
 # the most recent file in the log dir will be used
 
 # ================== COPYRIGHT ================================================
@@ -23,26 +22,54 @@
 #    James Roche (jroche@nygenome.org)
 #
 # ================== /COPYRIGHT ===============================================
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NC='\033[0m'
 
 init_wdl_dir=$(dirname "$0")
 cd ${init_wdl_dir}
 wdl_dir=$( pwd )
 
-    
+
+help_long="-h, --help          Show this help message and exit
+  --billing-export BILLING     The name of the table for the SQL query
+                               of the billing table.
+  --from-billing               Skip regenerating runtime metrics. Use existing
+                               metrics files; add the cost values from the billing
+                               table; and plot the results (including billing).
+  --gcp-project GCP_PROJECT    GCP project id for api queries.
+  --run-info-json RUNINFO_JSON Optional file that includes any sampleIds and the
+                               main workflow UUID.
+  --url URL                    Cromwell server URL (required)
+  --log-dir LOG_DIR            Output directory for all logs and reports
+                               related to this workflow UUID (required)
+  --project-name PROJECT_NAME  Project name associated with account.
+  --pairs-file PAIRS_FILE
+                               Optional, CSV file with items that are required to have
+                               \"tumor\", \"normal\" and \"pairId\" in the columns.
+                               Optionally, include \"tumorBam\", \"normalBam\" columns to create
+                               \"pairInfos\" and \"normalSampleBamInfos\" automatically.
+  --samples-file [SAMPLES_FILE]
+                               Not generally required. If tasks only require
+                               sampleId and do not use pairing information sample
+                               info can be populated with a CSV file. The CSV file
+                               requires a columns named [\"sampleId\"].
+
+"
+
 print_help() {
-  echo "USAGE: run_summary.sh -u URL -n PROJECT_NAME -g GCP_PROJECT -d log_dir [-r RUNINFO_JSON] [-i UUID] [-b BILLING EXPORT] [-p PAIRS_FILE] [-s SAMPLES_FILE]" >&2
-  echo "DESCRIPTION: submit workflow to cromwell." >&2
-  echo "Use -l for large jobs that may timeout." >&2
-  echo "Script requires jq, cromwell-tools, gcloud to be in the path." >&2
-  echo "Script also requires a default_credentials_JSON to be created by the user" >&2
-  echo "Run the following to generate a default credentials file " >&2
-  echo "$ gcloud auth application-default login" >&2
-  echo "# Script generates summary of any workflow." >&2
+  echo "USAGE: run_summary.sh -u URL -n PROJECT_NAME -g GCP_PROJECT -d log_dir [-r RUNINFO_JSON] [-i UUID] [-b BILLING EXPORT] [--from-billing] [-p PAIRS_FILE] [-s SAMPLES_FILE]" >&2
+  echo "DESCRIPTION: monitor or summarize cromwell workflow." >&2
+  echo "Script requires jq to be in the path." >&2
+  echo "If no *.RunInfo.json file OR uuid is provided then" >&2
+  echo "the most recent file in the log dir will be used" >&2
+  echo "Script generates summary of any workflow." >&2
+  echo "${help_long}" >&2
   exit 1
 }
 
 print_usage() {
-  echo "USAGE: run_summary.sh -u URL -n PROJECT_NAME -g GCP_PROJECT -d log_dir [-r RUNINFO_JSON] [-i UUID] [-b BILLING EXPORT] [-p PAIRS_FILE] [-s SAMPLES_FILE]" >&2
+  echo "USAGE: run_summary.sh -u URL -n PROJECT_NAME -g GCP_PROJECT -d log_dir [-r RUNINFO_JSON] [-i UUID] [-b BILLING EXPORT] [--from-billing] [-p PAIRS_FILE] [-s SAMPLES_FILE]" >&2
   exit 1
 }
 
@@ -53,6 +80,10 @@ for arg in "$@"; do
     -b|--billing-export)
         billing_export="$2"
         shift # Remove argument name from processing
+        shift # Remove argument value from processing
+        ;;
+    -z|--from-billing)
+        start_from_billing=1
         shift # Remove argument value from processing
         ;;
     -u|--url)
@@ -103,7 +134,7 @@ for arg in "$@"; do
 done
 
 if [ -z "$log_dir" ]; then
-    echo "Error: Missing required value for -l log dir (for run logs)" >&2
+    echo "Error: Missing required value for -d log dir (for run logs)" >&2
     print_usage
     exit 1
 fi
@@ -175,29 +206,63 @@ html="${log_dir}/${project_name}.${workflow_uuid}_outputMetrics.html"
 header="${log_dir}/${project_name}.${workflow_uuid}_outputMetrics.header.txt"
 costs_file="${log_dir}/${project_name}.${workflow_uuid}_outputCosts.csv"
 
-echo "Collect output files..."
-collect_command="time python ${wdl_dir}/tools/collect.py \
---run-data ${run_info_json} \
---url ${url} \
---output-info-file ${output_info_file} \
---gcp-project ${gcp_project}"
+if [ -z "$start_from_billing" ]; then
+    echo "Collect output files..."
+    collect_command="time python ${wdl_dir}/tools/collect.py \
+    --run-data ${run_info_json} \
+    --url ${url} \
+    --output-info-file ${output_info_file} \
+    --gcp-project ${gcp_project}"
 
-eval ${collect_command}
+    eval ${collect_command}
+fi
 
 if [[ ! -f $output_info_file ]]; then
     echo 'No output files were created by workflow so no outputInfo.json exists for further steps' >&2
     exit 0
 fi
 
-echo "Gather usage metrics..."
-runtime_command="time python ${wdl_dir}/tools/runtime.py \
-    --output-info-file ${output_info_file} \
-    --metrics-file-prefix ${log_dir}/${project_name}. \
-    --url ${url} \
-    --gcp-project ${gcp_project}"
+if [ -z "$start_from_billing" ]; then
+    echo "Gather usage metrics..."
+    runtime_command="time python ${wdl_dir}/tools/runtime.py \
+        --output-info-file ${output_info_file} \
+        --metrics-file-prefix ${log_dir}/${project_name}. \
+        --url ${url} \
+        --gcp-project ${gcp_project}"
 
-eval ${runtime_command}
+    eval ${runtime_command}
+fi
 
+# billing (optional)
+plot_metrics_file=${metrics_file}
+if [ ! -z "$billing_export" ]; then
+    echo "Adding cost per instance id to metrics..."
+    python ${wdl_dir}/tools/cost.py \
+    --output-metrics "${metrics_file}" \
+    --workflow-uuid "${workflow_uuid}" \
+    --gcp-project "${gcp_project}" \
+    --url "${url}"  \
+    --billing-export "${billing_export}" \
+    --out-file-prefix "${log_dir}/${project_name}.${workflow_uuid}"
+    
+    count=$(cat ${costs_file} |  wc -l)
+    
+    if [ "$count" -gt 1 ]; then
+        python ${wdl_dir}/tools/join.py \
+        ${workflow_uuid} \
+        ${costs_file} \
+        ${metrics_file} \
+        "${log_dir}/${project_name}.${workflow_uuid}"
+        # make plots with table that includes cost
+        plot_metrics_file="${log_dir}/${project_name}.${workflow_uuid}.outputMetrics.cost.csv"
+    else
+        echo -e "WARNING:${YELLOW}The billing-export query failed to find any matching records. Check to see if you are pointing to the correct table and using the correct gcp project.${NC}"
+    fi
+else
+    echo -e "WARNING:${YELLOW}Skipping adding cost per instance id to metrics because no --billing-export flag was used.${NC}"
+fi
+
+# plotting (optional)
 #skip if monitoring_image not in options
 monitored=$( cat ${output_info_file} | jq ".options.monitoring_image")
 if [ ! -z "$monitored" ]; then
@@ -205,7 +270,7 @@ if [ ! -z "$monitored" ]; then
     python ${wdl_dir}/tools/plot_runtime.py \
         --name ${project_name} \
         --output-info ${output_info_file} \
-        --metrics ${metrics_file} \
+        --metrics ${plot_metrics_file} \
         --non-retry-metrics ${non_retried_metrics_file} \
         --plot ${plot_file}
         
@@ -219,25 +284,4 @@ else
     echo "Skipping plot usage metrics because no monitoring image was declared in options json"
 fi
 
-    
-if [ ! -z "$billing_export" ]; then
-    echo "Adding cost per instance id to metrics..."
-    python ${wdl_dir}/tools/cost.py \
-    --output-metrics "${metrics_file}" \
-    --workflow-uuid "${workflow_uuid}" \
-    --gcp-project "${gcp_project}" \
-    --url "${url}"  \
-    --billing-export "${billing_export}" \
-    --out-file-prefix "${log_dir}/${project_name}.${workflow_uuid}"
-    
-    python ${wdl_dir}/tools/join.py \
-    ${workflow_uuid} \
-    ${costs_file} \
-    ${metrics_file} \
-    "${log_dir}/${project_name}.${workflow_uuid}"
-else
-    echo "Skipping adding cost per instance id to metrics because no --billing-export flag was used"
-
-
-fi
 
